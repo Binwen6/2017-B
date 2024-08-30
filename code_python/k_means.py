2a178ebe2c6db35fe9936148292a5f5f1639ccc3
eureka <l1045028202@163.com>
2024-08-30 18:16:35 +0800

    add

--- code_python/k_means.py
+++ code_python/k_means.py
@@ -1,7 +1,12 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# Set the random seed for reproducibility
+np.random.seed(42)
 
 df = pd.read_excel('../data/有效已结束项目任务位置数据.xlsx')
 # Standardize the data
@@ -30,15 +35,18 @@ plt.ylabel('Latitude')
 plt.title('K-means Clustering of Task Data')
 plt.show()
 
-# Save the plt
+# Save the plot
 fig.savefig('../fig/k_means.png')
+
+# 对于每个任务, 根据他的经纬度和他所属cluster的经纬度计算，一个距离存到新的一列distance中
+df['distance'] = 0
+for i in range(4):
+    points = df[df['cluster'] == i]
+    center = centers[i]
+    for index, row in points.iterrows():
+        distance = ((row['任务gps经度'] - center[0]) ** 2 + (row['任务gps 纬度'] - center[1]) ** 2) ** 0.5
+        df.loc[index, 'distance'] = distance
+        
 
 # 将每个任务所属的簇保存到文件data/有效已结束项目任务位置数据.xlsx
 df.to_excel('../data/有效已结束项目任务位置数据.xlsx', index=False)
-
-# 根据每个任务的经纬度计算该任务点与其所属簇的中心点的距离
-df['distance'] = 0
-for i in range(4):
-    df['distance'] = df.apply(lambda x: (x['任务gps经度'] - centers[i][0]) ** 2 + (x['任务gps 纬度'] - centers[i][1]) ** 2, axis=1)
-df.to_excel('../data/有效已结束项目任务位置数据.xlsx', index=False)
-
